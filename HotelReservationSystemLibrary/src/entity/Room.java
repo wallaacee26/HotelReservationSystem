1c894644d3d030c10f27275b39dffc4d0f1e7686
wallace <wallacephua26@gmail.com>
2024-11-05 22:27:08 +0800

    updated room/roomRate/roomType attributes

--- HotelReservationSystemLibrary/src/entity/Room.java
+++ HotelReservationSystemLibrary/src/entity/Room.java
@@ -1,10 +1,13 @@
 package entity;
 
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 /**
  *
@@ -18,6 +21,17 @@ public class Room implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long roomId;
 
+    @Column(nullable = false, unique = true)
+    private String roomNumber;
+    @Column(nullable = false)
+    private boolean available;
+    @Column(nullable = false)
+    private boolean disabled;
+
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    private RoomType roomType;
+        
     // default no-argument constructor for JPA
     public Room() {
         
@@ -29,6 +43,38 @@ public class Room implements Serializable {
 
     public void setRoomId(Long roomId) {
         this.roomId = roomId;
+    }
+    
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+    
+    public boolean isDisabled() {
+        return disabled;
+    }
+
+    public void setDisabled(boolean disabled) {
+        this.disabled = disabled;
+    }
+
+    public RoomType getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(RoomType roomType) {
+        this.roomType = roomType;
     }
 
     @Override
