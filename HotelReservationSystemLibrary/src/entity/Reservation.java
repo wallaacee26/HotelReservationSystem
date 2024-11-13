f33af157d7b29b972ed69ef74b8598088985ee52
wallace <wallacephua26@gmail.com>
2024-11-14 05:56:41 +0800

    made reservation entity uncacheable to avoid any stale read errors

--- HotelReservationSystemLibrary/src/entity/Reservation.java
+++ HotelReservationSystemLibrary/src/entity/Reservation.java
@@ -3,6 +3,7 @@ package entity;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.Cacheable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,6 +17,7 @@ import javax.persistence.OneToMany;
  * @author wallace
  */
 @Entity
+@Cacheable(false)
 public class Reservation implements Serializable {
 
     private static final long serialVersionUID = 1L;
